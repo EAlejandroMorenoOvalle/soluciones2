8cfe6a251b9da5259b82b0e6465e187a2190af98
EAlejandroMorenoOvalle <47793760+EAlejandroMorenoOvalle@users.noreply.github.com>
2020-06-03 00:05:33 -0400

    Add files via upload

---
+++ perl/2datoEscalar.pl
@@ -0,0 +1,9 @@
+my $numero = 3;
+my $loat = 3.1416;
+my $cadena = "hola";
+my $octal = 0377; # 3*8+7 = resultado *8 +7
+my $Hexadecimal = 0xff;
+
+print $cadena;
+print $octal;
+print $Hexadecimal;
---
+++ perl/3datoEscalar.pl
@@ -0,0 +1,15 @@
+
+use 5.010;
+
+#numero 
+my $numero = 3;
+#flotante 
+my $flotante = 3.1416;
+#cadena de caracteres
+my $cadena = "hola";
+
+print $cadena;
+
+my $octal =0377;
+print $octal;
+
